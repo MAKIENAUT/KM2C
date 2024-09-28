2d477ac604c206877a0f48e782b823603687525e
MAKIENAUT <mcrayescoto@gmail.com>
2024-09-28 16:15:38 +0800

    Initial Home Layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Sora } from "next/font/google";
+import { Sora, VT323 } from "next/font/google";
 import "./globals.css";
+import NavbarHero from "@/components/NavbarHero";
 
 const sora = Sora({ subsets: ["latin"] });
+const vt323 = VT323({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "SwiftForm",
@@ -18,9 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${sora.className} relative overflow-hidden bg-primary-neutral text-primary-white md:flex`}
+        className={`${sora.className} bg-primary-neutral text-primary-white relative flex-col overflow-x-hidden md:flex`}
       >
-        {children}
+        <div
+          className="w-full"
+          style={{
+            backgroundImage: `url('/Paris.jpg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <NavbarHero />
+          {children}
+        </div>
       </body>
     </html>
   );
