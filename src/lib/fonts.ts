0d82b062c4146595aef4d73d04b5534d1030f4fe
Sly Villafania <villafania.sly.lau@gmail.com>
2024-09-28 21:56:43 +0800

    Create lib fonts file to easily access km2c's fonts

---
+++ src/lib/fonts.ts
@@ -0,0 +1,5 @@
+import { VT323, Space_Grotesk } from "next/font/google";
+
+export const vt323 = VT323({ weight: ["400"], subsets: ["latin"] });
+
+export const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
