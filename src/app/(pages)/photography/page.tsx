3c0fd580f4326eb8e4a2f3edfe78f11e3eb84ad7
Sly Villafania <villafania.sly.lau@gmail.com>
2024-10-08 02:59:20 +0800

    Update photography page for carousel

--- src/app/(pages)/photography/page.tsx
+++ src/app/(pages)/photography/page.tsx
@@ -1,35 +1,46 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/atoms/carousel";
+import AutoScroll from "embla-carousel-auto-scroll";
 import FilmStrip from "@/components/FilmStrip";
 import { vt323 } from "@/lib/fonts";
 import { PHOTO_VALUES } from "@/lib/values";
 
 export default function PhotographyPage() {
   return (
-    <section className="h-[1000px] bg-cream py-2">
+    <section className="bg-cream py-2">
       <h1 className={`${vt323.className} mb-4 px-16 text-8xl text-maroon`}>
         PHOTOGRAPHY
       </h1>
-      <div className="no-scrollbar flex w-full overflow-x-scroll">
-        {PHOTO_VALUES.map(({ tailwindUrl, url, imgPosition, slug }) => (
-          <FilmStrip
-            key={url}
-            tailwindUrl={tailwindUrl}
-            imgPosition={imgPosition}
-            slug={slug}
-          />
-        ))}
-      </div>
-      <div className="no-scrollbar flex w-full flex-row-reverse overflow-x-scroll">
-        {PHOTO_VALUES.reverse().map(
-          ({ tailwindUrl, url, imgPosition, slug }) => (
-            <FilmStrip
-              key={url}
-              tailwindUrl={tailwindUrl}
-              imgPosition={imgPosition}
-              slug={slug}
-            />
-          )
-        )}
-      </div>
+      {PHOTO_VALUES.map(({ folderTitle, folderSrc, imgs, folderSlug }, i) => (
+        <div key={i} className="mb-16 flex flex-col gap-2">
+          <h1 className={`${vt323.className} px-2 text-6xl text-maroon`}>
+            {folderTitle}
+          </h1>
+          <Carousel
+            opts={{ dragFree: true, loop: true }}
+            plugins={[
+              AutoScroll({ stopOnInteraction: false, stopOnMouseEnter: true }),
+            ]}
+          >
+            <CarouselContent className="m-0">
+              {imgs.map(({ imgSlug, imgSrc, imgTitle }, i) => (
+                <CarouselItem key={i} className="p-0">
+                  <FilmStrip
+                    src={`${folderSrc}${imgSrc}`}
+                    title={imgTitle}
+                    slug={`${folderSlug}${imgSlug}`}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      ))}
     </section>
   );
 }
