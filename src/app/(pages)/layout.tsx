8f25b3c31b8f6ce1437361a2c2b9c36654d90633
Sly Villafania <villafania.sly.lau@gmail.com>
2024-10-11 13:57:27 +0800

    Move body tag out of main layout file and into children page and layout to allow for overflow-hidden property when the navbar menu is open

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { BaseNavbar } from "@/components/BaseNavbar";
+import { spaceGrotesk } from "@/lib/fonts";
+import { useState } from "react";
 
 export default function PageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <>
-      <BaseNavbar />
+    <body
+      className={`${spaceGrotesk.className} ${isMenuOpen ? "overflow-hidden" : ""} relative bg-black transition-all`}
+    >
+      <BaseNavbar
+        variant="solid"
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+      />
       {children}
-    </>
+    </body>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { HeroNavbar } from "@/components/HeroNavbar";
 import { Resume } from "@/components/Resume";
 import { useInView } from "@/hooks/useInView";
+import { spaceGrotesk } from "@/lib/fonts";
 
 export default function Home() {
   const contentRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   // Increase precision of thresholds near 1.0
   const thresholds = [
     ...Array.from({ length: 90 }, (_, i) => i / 100),
@@ -15,8 +17,16 @@ export default function Home() {
 
   return (
     <main className="w-full">
-      <HeroNavbar contentRef={contentRef} />
-      <Resume ref={contentRef} intersectionRatio={intersectionRatio} />
+      <body
+        className={`${spaceGrotesk.className} ${isMenuOpen ? "overflow-hidden" : ""} relative bg-black transition-all`}
+      >
+        <HeroNavbar
+          contentRef={contentRef}
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
+        />
+        <Resume ref={contentRef} intersectionRatio={intersectionRatio} />
+      </body>
     </main>
   );
 }
