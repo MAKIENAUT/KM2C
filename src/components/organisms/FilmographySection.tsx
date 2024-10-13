469f328a4d784e87860bde5e9b670dab3170a519
MAKIENAUT <mcrayescoto@gmail.com>
2024-10-13 23:00:56 +0800

    Last na

--- src/components/organisms/FilmographySection.tsx
+++ src/components/organisms/FilmographySection.tsx
@@ -207,7 +207,7 @@ const VideoGallerySection: React.FC = () => {
       <div
         className={`grid ${
           isGridView
-            ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+            ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3"
             : "grid-cols-1"
         } gap-4 sm:gap-6`}
       >
